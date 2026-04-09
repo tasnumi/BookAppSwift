abc43ad54284bb692bde6e11d9ed8b069ec9b519
ghibli-codes <grujicic.tamara04@gmail.com>
2026-04-08 21:19:34 -0700

    MapView Starter Code
    Added basic functionality of the map view including accessing user location and loading searched stores near them.

--- SwiftBookApp/ViewModel/MapViewModel.swift
+++ SwiftBookApp/ViewModel/MapViewModel.swift
@@ -6,3 +6,43 @@
 //
 
 import Foundation
+import Combine
+import CoreLocation
+import CoreLocationUI
+import SwiftUI
+import FirebaseAuth
+import FirebaseFirestore
+import Firebase
+// must make mapviewmodel of type NSObject and CLLocationManagerDelegate on top of being an
+// ObservableObject since it uses the CCLocationManager class (inhertied from NSObject and reports to CLLocationManagerDelegate)
+// https://developer.apple.com/documentation/corelocation/cllocationmanager
+class MapViewModel: NSObject, CLLocationManagerDelegate, ObservableObject {
+    // location manager must be used in order to call the requestlocation() method
+    // source is the location manager class from https://www.hackingwithswift.com/quick-start/swiftui/how-to-read-the-users-location-using-locationbutton
+    let manager = CLLocationManager()
+    // published var that stores the users current location
+    @Published var userCurrentLocation: CLLocationCoordinate2D?
+    
+    override init() {
+           super.init()
+           manager.delegate = self
+       }
+    // the function that requests the location from the user upon pressing the share location button
+    func requestUserLocation() {
+        // also the soltuion stated to to set the delegate to self
+        // directly in the function before calling request location
+        manager.delegate = self
+        manager.requestLocation()
+    }
+    // function that returns the coordinates of the user's current location
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        userCurrentLocation = locations.first?.coordinate
+    }
+    // getting an error that locationManager must respond to didFailWithError
+    // the reccomended solution is to add this event handler below to account for any errors in requesting location
+    // source https://stackoverflow.com/questions/40345170/delegate-must-respond-to-locationmanagerdidupdatelocations-swift-eroor
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+           print("Failed to get users location.")
+       }
+    
+}
