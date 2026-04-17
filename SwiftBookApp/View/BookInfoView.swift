fba6600ee3c551ac64284cbc02540d325ac2cc2d
ghibli-codes <grujicic.tamara04@gmail.com>
2026-04-17 12:41:06 -0700

    Book Detail View and Functionality
    Styled the book detail view which contains image, title, author, and expandable description. Allows the user to add/remove books from favorites and read list.

--- SwiftBookApp/View/BookInfoView.swift
+++ SwiftBookApp/View/BookInfoView.swift
@@ -8,6 +8,7 @@
 import Foundation
 import SwiftUI
 import FirebaseAuth
+import SwiftUI
 
 struct BookInfoView: View {
     @State var book: Book
@@ -15,41 +16,157 @@ struct BookInfoView: View {
     @StateObject private var bookInfoVM = BookDetailViewModel()
     
     @State private var description: String = ""
+    @State var isCollapsed = true
+    @State private var isBookFavorited: Bool = false
+    @State private var isBookRead: Bool = false
+    @State private var isEnabled = true
+   
     var body: some View {
+    ZStack {
+            Color("Background")
+                .ignoresSafeArea()
+
+            let thumbnail = book.volumeInfo.imageLinks?.thumbnail
+           
+            let URL = URL(string: thumbnail?.replacingOccurrences(of: "http://", with: "https://") ?? "")
         
-        VStack {
-            HStack(alignment: .top, spacing: 12){
-                let thumbnail = book.volumeInfo.imageLinks?.thumbnail
-                
-                let URL = URL(string: thumbnail?.replacingOccurrences(of: "http://", with: "https://") ?? "")
+            ScrollView(.vertical, showsIndicators: true) {
                 AsyncImage(url: URL) { image in
                     image.resizable()
                         .scaledToFill()
                 } placeholder: {
                     ProgressView()
                 }
-                .frame(width: 140, height: 200)
+                .frame(width: 230, height: 315)
                 .clipped()
                 .cornerRadius(8)
+                .padding(.bottom, 10)
+                .padding(.top, 30)
+
                 VStack(alignment: .leading, spacing: 6) {
-                    Text(book.volumeInfo.title ?? "Unknown Title")
-                    Text(book.volumeInfo.authors?.first ?? "Unknown Author")
-                        .font(.subheadline)
-                        .foregroundColor(.gray)
-                }
+                    Text("Title: \(book.volumeInfo.title ?? "Unknown Title")").font(.system(size: 19))
+                    Text("Author: \(book.volumeInfo.authors?.first ?? "Unknown Author")").font(.system(size: 19))
+//                        .font(.subheadline)
+//                        .foregroundColor(.gray)
+                   // Text("Description: \(book.volumeInfo.description ?? "No description available.")")
+                    Text("Description: \(book.volumeInfo.description ?? "No description available.")").font(.system(size: 19)).truncateText(length: 5, isEnabled: isEnabled, animation: .smooth(duration: 0.5, extraBounce: 0)).onTapGesture {
+                        isEnabled.toggle()
+                    }.frame(maxWidth: .infinity, alignment: .leading)
+                }.padding(.horizontal, 20)
             }
-            Text(book.volumeInfo.description ?? "No description available.")
-            Button("Add to Favorites") {
-                //https://firebase.google.com/docs/auth/web/manage-users
-                //get the currently logged in user using Firebase Auth documentation
-                bookInfoVM.addToFavorites(book: book, userId: Auth.auth().currentUser?.uid ?? "")
-            }
-        }
-        
+    }.onAppear{
+        bookInfoVM.isInFavorites(currentBook: book, userId:  Auth.auth().currentUser?.uid ?? "")
+        bookInfoVM.isInRead(currentBook: book, userId:  Auth.auth().currentUser?.uid ?? "")
     }
+    .toolbar {
+        // use toolbaritemgroup with spacing to format https://www.hackingwithswift.com/forums/swiftui/spacing-toolbar-elements/7285
+          ToolbarItemGroup(placement: .bottomBar) {
+              Button {
+                  // if the book is already in their favorites, remove it
+                  if(bookInfoVM.isInFav) {
+                      print("book removed from favorites")
+                      // remove from favorites
+                      bookInfoVM.removeFromFavorites(currentBook: book, userId: Auth.auth().currentUser?.uid ?? "")
+                  }
+                  else {
+                      print("book saved to favorites")
+                      bookInfoVM.addToFavorites(book: book, userId: Auth.auth().currentUser?.uid ?? "")
+                  }
+              } label: {
+                  // modify the image conditionally between filled and unfilled based on
+                  // the if book is in favorites
+                  if(bookInfoVM.isInFav) {
+                      Image(systemName: "heart.fill").foregroundStyle(.red)
+                  }
+                  else {
+                      Image(systemName: "heart")
+                  }
+              }
+              Button {
+                  if(bookInfoVM.isInRead) {
+                      print("book removed from read")
+                      // remove from read books
+                      bookInfoVM.removeFromRead(currentBook: book, userId: Auth.auth().currentUser?.uid ?? "")
+                  }
+                  else {
+                      print("book marked as read")
+                      bookInfoVM.markAsRead(book: book, userId: Auth.auth().currentUser?.uid ?? "")
+                  }
+              } label: {
+                  if(bookInfoVM.isInRead) {
+                      Image(systemName: "bookmark.fill")
+                  }
+                  else {
+                      Image(systemName: "bookmark")
+                  }
+              }
+              Spacer()
+          }
+      }
+    }
+    
+   
+}
+// following this tutorial https://www.youtube.com/watch?v=gFn6LgDPx5g
+// on how to make an extention that will enable and animate text truncation for long book descriptions
+// this function trucateText is called on the description text, users have the choice to expand it by clicking the text to toggle
+extension Text {
+    @ViewBuilder
+    func truncateText(length: Int, isEnabled: Bool, animation: Animation) -> some View {
+        self
+            .modifier(TruncateTextViewModifier(length: length, isEnabled: isEnabled, animation: animation))
+    }
+}
+struct TruncateTextViewModifier: ViewModifier {
+    var length: Int
+    var isEnabled: Bool
+    var animation: Animation
+    @State private var limitedSize: CGSize = .zero
+    @State private var fullSize: CGSize = .zero
+    @State private var animatedProgress: CGFloat = 0
+    func body(content: Content) -> some View {
+        content
+            .lineLimit(length)
+            .opacity(0)
+            .fixedSize(horizontal: false, vertical: true)
+            .onGeometryChange(for: CGSize.self) {
+                $0.size
+            } action: {newSize in
+                limitedSize = newSize
+            }
+            .frame(height: isExpanded ? fullSize.height: nil)
+            .overlay {
+                GeometryReader {
+                    let contentSize = $0.size
+                    
+                    content
+                        . fixedSize(horizontal: false, vertical: true)
+                        .onGeometryChange(for: CGSize.self) {
+                            $0.size
+                        } action: {newSize in
+                            fullSize = newSize
+                        }
+                        .frame(maxWidth: contentSize.width, maxHeight: contentSize.height, alignment: isExpanded ? .center : .top)
+                }
+                
+            }
+            .clipped()
+            .onChange(of: isEnabled) { oldVal, newVal in
+                withAnimation(animation) {
+                    animatedProgress = !newVal ? 1 : 0
+                }
+                
+            }.onAppear {
+                animatedProgress = !isEnabled ? 1 : 0
+            }
+    }
+    var isExpanded: Bool {
+        animatedProgress == 1
+    }
+    
 }
 
 //display the book info page
-#Preview {
-    BookInfoView(book: Book(volumeInfo: VolumeInfo(title: "hello", authors: ["hello"], description: "Hello", imageLinks: ImageLinks(thumbnail: "hi"))))
-}
+//#Preview {
+//    BookInfoView(book: Book(volumeInfo: VolumeInfo(title: "hello", authors: ["hello"], description: "Hello", imageLinks: ImageLinks(thumbnail: "hi"))))
+//}
--- SwiftBookApp/ViewModel/BookDetailViewModel.swift
+++ SwiftBookApp/ViewModel/BookDetailViewModel.swift
@@ -12,6 +12,9 @@ import FirebaseFirestore
 
 class BookDetailViewModel: ObservableObject {
     
+    @Published var isInFav: Bool = true
+    @Published var isInRead: Bool = true
+    
     func addToFavorites(book: Book, userId: String) {
         let db = Firestore.firestore()
         
@@ -23,6 +26,7 @@ class BookDetailViewModel: ObservableObject {
         ]
         
         db.collection("users").document(userId).collection("favorites").document(book.id).setData(data)
+        self.isInFav = true
         //go to the database called users
         //enter the document called userid to target a specific user
         //add a subcollection called favorites
@@ -30,5 +34,87 @@ class BookDetailViewModel: ObservableObject {
         //add the data as fields in this
         //resource: https://firebase.google.com/docs/firestore/data-model
         
+    }
+    // functionality for the user to mark the book as read
+    func markAsRead(book: Book, userId: String) {
+        let db = Firestore.firestore()
+        
+        let data: [String: Any] = [
+            "title": book.volumeInfo.title ?? "",
+            "author": book.volumeInfo.authors?.first ?? "",
+            "cover": book.volumeInfo.imageLinks?.thumbnail ?? "",
+            "description": book.volumeInfo.description ?? ""
+        ]
+        
+        // add to a subcollection called readBooks
+        db.collection("users").document(userId).collection("readBooks").document(book.id).setData(data)
+        self.isInRead = true
+    }
+    
+    // function to check if the book already exists in the collection
+    // reference on how to check if a document already exists using firebase
+    // and swift https://stackoverflow.com/questions/48224143/swift-firestore-check-if-documents-exist
+    func isInFavorites(currentBook: Book, userId: String) {
+        let db = Firestore.firestore()
+        
+        let docRef = db.collection("users").document(userId).collection("favorites").document(currentBook.id)
+        
+        docRef.getDocument { (document, error) in
+            // must unwrap optionals in the if statement or it gives an error  https://stackoverflow.com/questions/24548999/unwrapping-multiple-optionals-in-if-statement
+            if let document = document, document.exists {
+                self.isInFav = true
+            }
+            else {
+                self.isInFav = false
+            }
+            
+        }
+    }
+    
+    // check for if the book is in read
+    func isInRead(currentBook: Book, userId: String) {
+        let db = Firestore.firestore()
+        
+        let docRef = db.collection("users").document(userId).collection("readBooks").document(currentBook.id)
+        
+        docRef.getDocument { (document, error) in
+            if let document = document, document.exists {
+                self.isInRead = true
+            }
+            else {
+                self.isInRead = false
+            }
+            
+        }
+    }
+    
+    func removeFromFavorites(currentBook: Book, userId: String) {
+        let db = Firestore.firestore()
+        
+        // function to delete a document in firestore https://www.hackingwithswift.com/forums/swiftui/firestore-delete/8831
+        let docRef = db.collection("users").document(userId).collection("favorites").document(currentBook.id)
+        docRef.delete() { (error) in
+            if let error = error {
+                print("error removing document \(error)")
+            } else {
+                print("document successfully removed from favorites")
+                self.isInFav = false
+            }
+        }
+    }
+
+    func removeFromRead(currentBook: Book, userId: String) {
+        let db = Firestore.firestore()
+        
+        let docRef = db.collection("users").document(userId).collection("readBooks").document(currentBook.id)
+        
+        docRef.delete() { (error) in
+            if let error = error {
+                print("error removing document \(error)")
+            } else {
+                print("document successfully removed from read books")
+                self.isInRead = false
+            }
+        }
     }
 }
