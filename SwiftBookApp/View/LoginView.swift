2280ea288184d18c8807e36d5767e156f39cfe81
Tasnim Haque <tasnimsgames@gmail.com>
2026-04-17 02:49:40 -0700

    feat: signup/login page styles
    styled the login/signup pages
    
    added more error states and displaying it on the page
    
    querying for the current logged in users username to display it on the main homepage
    
    //TODO:
    - style the list view of the result books
    - only display the unique ID of books in the list

--- SwiftBookApp/View/LoginView.swift
+++ SwiftBookApp/View/LoginView.swift
@@ -12,31 +12,96 @@ struct LoginView: View {
     @StateObject var loginVM = LoginViewModel()
     @EnvironmentObject var mainVM: MainAppViewModel
     var body: some View {
-        Text("Login")
-            .font(Font.largeTitle)
-        
-        VStack {
-            HStack {
-                Text("Enter Email")
-                TextField("", text: $loginVM.email)
-                    .textFieldStyle(.roundedBorder)
-                    .padding()
+        ZStack {
+            Color("Background")
+                .ignoresSafeArea()
+            VStack(spacing: 30) {
+                Text("Welcome")
+                    .padding(25)
+                    .font(Font.largeTitle)
+                    .foregroundStyle(Color("GreenButton"))
+                    .foregroundColor(.white)
+                    .cornerRadius(15)
+                    .bold()
+                VStack(alignment: .leading, spacing: 8) {
+                    Text("Enter Email")
+                        .font(Font.title2)
+                        .bold()
+                        .padding(15)
+                    HStack {
+                        Image(systemName: "envelope")
+                                .foregroundColor(.gray)
+                        TextField("", text: $loginVM.email)
+                            .textFieldStyle(.plain)
+                    }
+                    .padding(10)
+                    .background(Color(.systemGray6))
+                    .cornerRadius(12)
+                    .overlay (
+                        RoundedRectangle(cornerRadius: 12)
+                            .stroke(Color.gray.opacity(0.3), lineWidth: 1)
+                    )
+                    .padding(.horizontal)
+                    .frame(alignment: .leading)
+                    
+                }
+                .padding(.leading, 20)
+                
+                VStack(alignment: .leading, spacing: 8) {
+                    Text("Enter Password")
+                    .font(Font.title2)
+                    .bold()
+                    .padding(15)
+                    HStack {
+                        Image(systemName: "lock")
+                            .foregroundColor(.gray)
+                        TextField("", text: $loginVM.password)
+                            .textFieldStyle(.plain)
+                    }
+                    .padding(10)
+                    .background(Color(.systemGray6))
+                    .cornerRadius(12)
+                    .overlay (
+                        RoundedRectangle(cornerRadius: 12)
+                            .stroke(Color.gray.opacity(0.3), lineWidth: 1)
+                    )
+                    .padding(.horizontal)
+                    .frame(alignment: .leading)
+                    
+                }
+                .padding(.leading, 20)
+                
+                if !loginVM.hasError.isEmpty {
+                    Text(loginVM.hasError)
+                        .foregroundColor(.red)
+                }
+                
+                Button("Login") {
+                    if (!loginVM.email.isEmpty || !loginVM.password.isEmpty) {
+                        loginVM.login(mainVM: mainVM)
+                    }
+                    print(mainVM.isLoggedIn)
+                }
+                .font(Font.title3)
+                .padding(.horizontal, 25)
+                .padding(.vertical, 15)
+                .bold()
+                .background(Color("GreenButton"))
+                .foregroundColor(.white)
+                .cornerRadius(10)
+                
+                Button("Don't have an account? Signup") {
+                    mainVM.signedUp = false
+                    mainVM.isLoggedIn = false
+                }
+                .font(Font.title3)
             }
             
-            HStack {
-                Text("Enter Password")
-                TextField("", text: $loginVM.password)
-                    .textFieldStyle(.roundedBorder)
-                    .padding()
-            }
         }
-        if !loginVM.hasError.isEmpty {
-            Text(loginVM.hasError)
-                .foregroundColor(.red)
-        }
-        Button("Login") {
-            loginVM.login(mainVM: mainVM)
-            print(mainVM.isLoggedIn)
-        }
+        
+        
     }
+}
+#Preview {
+    LoginView()
 }
