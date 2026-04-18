600734194dd4bf1f4b847df932381636d63ff4aa
ghibli-codes <grujicic.tamara04@gmail.com>
2026-04-17 22:34:15 -0700

    Profile View
    Implemented the profile view for the users which displays their favorite and read books. Users can click on the books and go to the book information page for each one.

--- SwiftBookApp/ViewModel/ProfileViewModel.swift
+++ SwiftBookApp/ViewModel/ProfileViewModel.swift
@@ -6,3 +6,77 @@
 //
 
 import Foundation
+import Combine
+import FirebaseAuth
+import SwiftUI
+import FirebaseFirestore
+
+class ProfileViewModel: ObservableObject {
+    
+    // array to hold favorite books
+    @Published var favoriteBooks: [Book] = []
+    // array to hold read books
+    @Published var readBooks: [Book] = []
+    
+    // fetch data from firebase resource used https://peterfriese.dev/blog/2020/swiftui-firebase-fetch-data/
+    // and https://www.youtube.com/watch?v=XvOgTmG86FE
+    
+    func fetchFavBookData() async {
+        let db = Firestore.firestore()
+        
+        db.collection("users").document(Auth.auth().currentUser?.uid ?? "").collection("favorites").addSnapshotListener { (querySnapshot, error) in
+          guard let documents = querySnapshot?.documents else {
+            print("No documents")
+            return
+          }
+            self.favoriteBooks = documents.map { queryDocumentSnapshot -> Book in
+                let data = queryDocumentSnapshot.data()
+                // as defined in the model, if the volume info title is "" use the document id as the id
+                // how to check if a string is empty https://stackoverflow.com/questions/29381994/check-string-for-nil-empty
+                let title: String
+                if let stringCheck = data["title"] as? String, !stringCheck.isEmpty {
+                    // if it is not empty, set title to data["title"]
+                     title = stringCheck
+                }
+                else {
+                    // else set title as the document id to use as the book identifier from profile view
+                     title = queryDocumentSnapshot.documentID
+                }
+                //let title = data["title"] as? String ??
+                let authors = data["authors"] as? [String] ?? []
+                let description = data["description"] as? String ?? ""
+                // in the favorites and readBooks collection, the image url is stored as the string "cover"
+                // imagelinks structure from the source below
+                // https://developers.google.com/books/docs/v1/reference/volumes
+                let cover = data["cover"] as? String ?? ""
+                return Book(volumeInfo: VolumeInfo(title: title, authors: authors, description: description, imageLinks: ImageLinks(thumbnail: cover)))
+            }
+        }
+    }
+  
+    func fetchReadBookData() async {
+        let db = Firestore.firestore()
+        
+        db.collection("users").document(Auth.auth().currentUser?.uid ?? "").collection("readBooks").addSnapshotListener { (querySnapshot, error) in
+          guard let documents = querySnapshot?.documents else {
+            print("No documents")
+            return
+          }
+            self.readBooks = documents.map { queryDocumentSnapshot -> Book in
+                let data = queryDocumentSnapshot.data()
+                let title: String
+                if let stringCheck = data["title"] as? String, !stringCheck.isEmpty {
+                     title = stringCheck
+                }
+                else {
+                     title = queryDocumentSnapshot.documentID
+                }
+                //let title = data["title"] as? String ?? ""
+                let authors = data["authors"] as? [String] ?? []
+                let description = data["description"] as? String ?? ""
+                let cover = data["cover"] as? String ?? ""
+                return Book(volumeInfo: VolumeInfo(title: title, authors: authors, description: description, imageLinks: ImageLinks(thumbnail: cover)))
+            }
+        }
+    }
+}
